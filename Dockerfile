746b9df619d228b815fdf1e37dd60ee45d722050
Pedro Trujillo <pedroetb@gmail.com>
2018-11-14 18:56:42 +0000

    Add initial application set

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM registry.gitlab.com/redmic-project/docker/index-pages:latest
+
+LABEL maintainer="pedroetb@gmail.com"
+
+COPY content content/
---
+++ content/config.js
@@ -0,0 +1,27 @@
+var config = {
+	"domain": "pedroetb.casacam.net",
+	"protocol": "https",
+	"resourcesPath": "content/resources/",
+	"title": "Esto es pedroetb.casacam.net",
+	"items": [{
+		"subdomain": "traefik",
+		"img": "traefik.png",
+		"title": "Traefik",
+		"description": "Dashboard de Traefik, proxy inverso dinámico de contenedores y servidor web"
+	},{
+		"subdomain": "portainer",
+		"img": "portainer.png",
+		"title": "Portainer",
+		"description": "Gestor de contenedores y entorno Docker"
+	},{
+		"subdomain": "tts",
+		"img": "speech.png",
+		"title": "TTS-API",
+		"description": "API REST y aplicación de TTS (texto a voz), para hacerte oír"
+	},{
+		"subdomain": "tajinaste",
+		"img": "tajinaste.svg",
+		"title": "Tajinaste Manager",
+		"description": "Aplicación de gestión de la escuela JPC Tajinaste"
+	}]
+};
